de7164abe97a90ee0e59b461d458b265cf174801
이진곤 <qazqww@naver.com>
2022-09-02 18:44:13 +0900

    initial setting

---
+++ junit-project/src/test/java/study/raptor/junitproject/JunitProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package study.raptor.junitproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunitProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
